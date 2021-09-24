0ee60f1ad0d2d2e1674ef0a96c4117c7647ddc1a
quyetfoxdevfront12 <lequyet840500@gmail.com>
2021-09-24 23:37:47 +0700

    update page products

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePages from "../pages/homepages";
-import Contracts from  "../pages/contracts"
+import Contracts from "../pages/contacts";
+import Products from "../pages/products";
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/">
-             <HomePages/>
+        <Route path="/" exact>
+          <HomePages />
         </Route>
-          <Route path="/contract">
-              <Contracts></Contracts>
-          </Route>
+        <Route path="/contact">
+          <Contracts></Contracts>
+        </Route>
+        <Route path="/products">
+          <Products/>
+        </Route>
       </Switch>
     </Router>
   );
