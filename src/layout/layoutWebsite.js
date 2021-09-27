a65f18d03b5f03bc091ae0c2f9f802b1ff0a72c5
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2021-09-27 21:20:10 +0700

    Tôi sửa lại giao diện một ít

--- src/layout/layoutWebsite.js
+++ src/layout/layoutWebsite.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LayoutWebsite = ({children}) => {
     return (
-        <div className="container tw-w-full md:tw-max-w-full tw-mx-auto tw-p-3">
+        <div className="container tw-w-full md:tw-max-w-full tw-mx-auto">
             {children}
         </div>
     )
