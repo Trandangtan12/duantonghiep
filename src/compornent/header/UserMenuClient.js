8ef21dbe18f3ef6e5d698c31e70d41edae65b596
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2021-11-14 10:12:50 +0700

    update

--- src/compornent/header/UserMenuClient.js
+++ src/compornent/header/UserMenuClient.js
@@ -45,17 +45,7 @@ const UserMenuClient = () => {
                   </Link>
                 )}
               </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'tw-bg-green-600 tw-text-white' : 'tw-text-gray-900'
-                    } group tw-flex tw-rounded-md tw-items-center tw-w-full tw-px-2 tw-py-2 tw-text-sm`}
-                  >
-                  Đổi mật khẩu                 
-                  </button>
-                )}
-              </Menu.Item>
+             
               {!isLogged && (
                   <>
                    <Menu.Item>
@@ -87,6 +77,18 @@ const UserMenuClient = () => {
                </>
               )}
               {isLogged && (
+                <>
+                 <Menu.Item>
+                 {({ active }) => (
+                   <button
+                     className={`${
+                       active ? 'tw-bg-green-600 tw-text-white' : 'tw-text-gray-900'
+                     } group tw-flex tw-rounded-md tw-items-center tw-w-full tw-px-2 tw-py-2 tw-text-sm`}
+                   >
+                   Đổi mật khẩu                 
+                   </button>
+                 )}
+               </Menu.Item>
                 <Menu.Item>
                 {({ active }) => (
                     <Link to="/signin" onClick={()=> UserApi.signout(() => {setIsLogged(false); history.push("/")})}>
@@ -100,6 +102,7 @@ const UserMenuClient = () => {
                   </Link>
                 )}
               </Menu.Item>
+              </>
               )}
               
              
