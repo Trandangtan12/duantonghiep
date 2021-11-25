38660ca2b4532edd8fd388dbd182252740890174
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2021-11-25 22:55:11 +0700

    update

--- src/pages/public/ticketPage/tabList.js
+++ src/pages/public/ticketPage/tabList.js
@@ -13,6 +13,7 @@ const TabList = () => {
   const listWaiting = availableOrder.filter(item => item.user_id == user.id && item.status == "WAITING_ACTIVE");
   const listActived = availableOrder.filter(item => item.user_id == user.id && item.status == "ACTIVED");
   const listRejected = availableOrder.filter(item => item.user_id == user.id && item.status == "REJECTED");
+  const listDeposit = availableOrder.filter(item => item.user_id == user.id && item.status == "DEPOSIT");
   return (
     <div className="tw-w-full sm:tw-px-0 tw-bg-white tw-rounded-lg">
       <Tab.Group>
@@ -31,6 +32,11 @@ const TabList = () => {
             selected ? 'tw-mr-14 tw-py-4 tw-border-b-2 tw-border-black' : 'tw-mr-14 tw-py-4 tw-bg-white tw-text-black'
           }>
               Chuyến đã hủy
+            </Tab>
+            <Tab className={({ selected }) =>
+            selected ? 'tw-mr-14 tw-py-4 tw-border-b-2 tw-border-black' : 'tw-mr-14 tw-py-4 tw-bg-white tw-text-black'
+          }>
+              Đặt cọc
             </Tab>
         </Tab.List>
         <Tab.Panels className="tw-p-4">
@@ -80,6 +86,23 @@ const TabList = () => {
                 </div>
                 <div className="col">
                   <p className="tw-bg-red-600 tw-text-white tw-p-1">Đã hủy</p>
+                </div>
+              </div>
+              </div>
+              )}
+            </Tab.Panel>
+            <Tab.Panel>
+            {listDeposit.map(items => <div className="tw-mb-3">
+              <div className="tw-font-bold tw-pb-3">{items.buses.date_active}</div>
+              <div className="tw-flex tw-justify-between tw-border tw-border-black tw-rounded-lg tw-p-4">
+                <div className="col">
+                  <p className="tw-text-3xl tw-font-black tw-pb-2">{items.buses.start_time}</p>
+                  <p className="tw-text-sm">{items.buses.name}</p>
+                  <p className="tw-text-sm">Số lượng: {items.quantity} vé</p>
+                  <p className="tw-text-sm">Giá: {new Intl.NumberFormat('vi', {  currency: 'VND', style: 'currency',}).format(items.totalPrice)}</p>
+                </div>
+                <div className="col">
+                <p className="tw-bg-blue-400 tw-text-black tw-p-1">Đã đặt cọc</p>
                 </div>
               </div>
               </div>
