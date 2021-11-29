b857cf6ea07414d8649c99c801d10253b87da5c5
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2021-11-29 20:13:55 +0700

    update plugin chat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Routes from "./router";
 import 'alertifyjs/build/css/alertify.css';
 import  './App.css'
-import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
-  return <> <Routes />  
-  <MessengerCustomerChat pageId="104419498749033"  appId="499856621131811"/>
-  </>;
+  return  <Routes />  
+ 
 }
 
 export default App;
