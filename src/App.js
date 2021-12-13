df15b600b449281191ba1c2b3a934922f469ff32
quyetfoxdevfront12 <lequyet840500@gmail.com>
2021-12-13 23:30:15 +0700

    update statistical

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Routes from "./router";
 import 'alertifyjs/build/css/alertify.css';
 import  './App.css'
-
+import "react-datepicker/dist/react-datepicker.css";
 function App() {
   return  <Routes />  
  
