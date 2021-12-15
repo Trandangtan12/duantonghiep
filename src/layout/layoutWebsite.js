abf85f8485a5ea4d3556c34a1b0e02df8d0ef160
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2021-12-15 22:22:22 +0700

    update

--- src/layout/layoutWebsite.js
+++ src/layout/layoutWebsite.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { isMobile } from 'mobile-device-detect';
 import MobileComponent from './MobileComponent'
 import DesktopComponent from "./DesktopComponent"
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-const LayoutWebsite = ({children}) => {
-    return (
-      <>
-      {isMobile ? <MobileComponent children={children}/> : <DesktopComponent children={children}/>}
-      </>
-    )
+const LayoutWebsite = ({ children }) => {
+  return (
+    <>
+      <MessengerCustomerChat
+        pageId="104419498749033"
+        appId="499856621131811"
+      />
+      {isMobile ? <MobileComponent children={children} /> : <DesktopComponent children={children} />}
+    </>
+  )
 }
 
 export default LayoutWebsite
