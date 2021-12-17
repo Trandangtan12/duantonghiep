b9bae8619e2fede40726fde2bcc78335f92dd151
quyetfoxdevfront12 <lequyet840500@gmail.com>
2021-12-17 20:25:02 +0700

    update notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,21 @@
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js');
+   importScripts('https://www.gstatic.com/firebasejs/8.9.0/firebase-analytics.js');
+   importScripts('https://www.gstatic.com/firebasejs/8.9.0/firebase-messaging.js')
 
+   if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  }
+   
 // Initialize the Firebase app in the service worker by passing the generated config
 var firebaseConfig = {
-  apiKey: "AIzaSyCWR_9cNDnSpo59c-YCeC6BBi2IVRyEHyw",
-  authDomain: "headphone-899cc.firebaseapp.com",
-  projectId: "headphone-899cc",
-  storageBucket: "headphone-899cc.appspot.com",
   messagingSenderId: "975118271133",
   appId: "1:975118271133:web:7fa12577c88bd498e00a74",
-  measurementId: "G-3RCEQ439XG",
 };
 
 firebase.initializeApp(firebaseConfig);
