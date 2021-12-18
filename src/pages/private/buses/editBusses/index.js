81dccb54ccfac69fb745e926850b5ea297e97da5
quyetfoxdevfront12 <lequyet840500@gmail.com>
2021-12-18 22:38:58 +0700

    update permission side bar

--- src/pages/private/buses/editBusses/index.js
+++ src/pages/private/buses/editBusses/index.js
@@ -95,7 +95,7 @@ const EditBusses = () => {
     setValue("start_time", startTime);
   };
   const handleChangeEndTime = (date) => {
-    const endTime = moment(date).format("HH:mm");
+    const endTime = moment(date).toISOString();
     setEndDate(date);
     setValue("end_time", endTime);
   };
